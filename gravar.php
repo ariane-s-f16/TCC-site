4783e16dbfb73c5833a0cd933bfeb218d3410ef1
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-02-17 11:08:52 -0300

    continuar o arquivo gravar.php

---
+++ gravar.php
@@ -0,0 +1,13 @@
+<!--definir um caminho absoluto para armazenar os dados no arquivon de texto na variável $nomedoarquivo-->
+<?php
+$nome = isset($_POST['nome'])? $_POST['nome'] :NULL;
+$email = isset($_POST['email'])? $_POST['email'] :NULL;
+$senha = isset($_POST['senha'])? $_POST['senha'] :NULL;
+
+
+if(! is_null ($nome)){
+    $nomedoarquivo  
+}
+
+
+?>
