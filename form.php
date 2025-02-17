ff8aa478c9d038a12e24cf668bd496d7768af61b
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-02-17 15:58:49 -0300

    falta documetar

--- form.php
+++ form.php
@@ -11,9 +11,15 @@
             <div class="campo1">Nome <input type="text" name="nome" value="<?php $nome ?>" autofocus="autofocus"/></div>
             <div class="campo2">email<input type="text" name="email" value="<?php $email ?>"/></div>
             <div class="campo3">senha<input type="password" name="senha" value="<?php $senha ?>"/></div>
+            <br><br>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="areatexto">Descrição</label>
+                    <div class="col-md-4">                     
+                        <textarea class="form-control"  name="descricao" value="<?php $descricao ?>">Escreva aqui</textarea>
+                    </div>
+            </div>
         </div>
-       
-
-
+    
 </body>
 </html>
--- gravar.php
+++ gravar.php
@@ -1,13 +1,61 @@
-<!--definir um caminho absoluto para armazenar os dados no arquivon de texto na variável $nomedoarquivo-->
+<!--passo a passo(lógica)-->
+<!--(1) pegar os dados enviados pelo método POST -->
+<!--(1.2-) verificar se o nome do aluno foi enviado, caso n for, nd vai acontecer e e a página inicial vai ser exibida-->
+<!--(1.5-)definir um caminho absoluto para armazenar os dados no arquivon de texto na variável $nomedoarquivo--->
+<!--(2)criar o arquivo 'usuarios.txt' se ele não existir e garantir q ele tem permissão para ser gravado-->
+<!--(3)abrir o arquivo 'usuarios.txt' e colocar no modo de leitura-->
+<!--(4)verificar se a requisição é para inclução ou para alterar-->
+<!--(5)caso seja uma inclusão -->
+
 <?php
+define('DS', DIRECTOR_SEPARATOR);
 $nome = isset($_POST['nome'])? $_POST['nome'] :NULL;
 $email = isset($_POST['email'])? $_POST['email'] :NULL;
 $senha = isset($_POST['senha'])? $_POST['senha'] :NULL;
 
 
 if(! is_null ($nome)){
-    $nomedoarquivo  
+    $nomedoarquivo= __DIR__.DS."usuarios.txt";
+    require 'arquivo.php';
+    $handle= fopen($nomedoarquivo, 'r');
+
+    if(empty($email)){
+        $ultimoemail = 0;
+        while(! feof($handle)){
+            $recordar= explode(',', fread($handle,80));
+            $ultimoemail=(isset($recordar[0]) && empty($recordar[0])) ? $ultimoemail : $recordar[0];
+        }
+
+    }
+    fclose($handle);
+    $handle= fopen(__DIR__.DS.'usuarios.txt', 'a');
+    $email= $ultimoemail + 1;
+    fwrite($handle, substr($email.','.$nome.','. str_repeat('', 80), 0, 78) . ",\n",80);
+    fclose($handle);
+
+}else {
+    $arquivotemporario = __DIR__.DS."usuarios.txt";
+    $handletemporario = fopen($arquivotemporario, 'w');
+    while(! feof($handle)){
+        $row= fread($handle, 80);
+        $recordar= explode(',', 80);
+        $ultimoemail=(isset($recordar[0]) && empty($recordar[0])) ? $ultimoemail : $recordar[0];
+        if(!$ultimoemail = $email){
+            fwrite($handletemporario, substr($email. ','. $nome.','.str_repeat('', 80), 0, 78) .",\n",80 );
+
+        }else{
+            fwrite($handletemporario, $row);
+        }
+        fclose($handletemporario);
+        fclose($handle);
+        unlink($arquivotemporario);
+        copy($arquivotemporario, $nomedoarquivo);
+        
+    }
+    fclose($handle);
 }
+header ('Location: index.php ');
+
 
 
 ?>
