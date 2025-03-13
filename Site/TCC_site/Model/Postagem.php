4e9362cd26a5a573e500113b5067c316be3f136d
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-03-13 15:13:01 -0300

    sdf

--- Site/TCC_site/Model/Postagem.php
+++ Site/TCC_site/Model/Postagem.php
@@ -1,8 +1,25 @@
 <?php
 
-public static class Selecionatd()
+public class Postagem
 {
-    $conexao= new 
+    public static function Selecionatd()
+    {
+        $conexao= new PDO('msql: host:localhost; dbname:tcc;', 'root:','');
+
+        $var_dump= ($conexao);
+
+       $res= Postagem:: Selecionatd2();
+       $var_dump=($res);
+    }
+
+    public static function Selecionatd2()
+    {
+        $conexao= new PDO('msql: host:localhost; dbname:tcc;', 'root:','');
+
+        $var_dump= ($conexao);
+
+     
+    }
 }
 
 ?>
