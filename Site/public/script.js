7a31145f85e675b26901a43808c2ecfdc37a10d7
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-11-17 10:38:16 -0300

    WDD

--- Site/public/script.js
+++ Site/public/script.js
@@ -38,6 +38,48 @@ document.addEventListener('DOMContentLoaded', () => {
         input.addEventListener('input', () => limparErro(input));
     });
 });
+//============avaliação===============================
+const btn_sobre = document.getElementById('btn_navperfil-sobre');
+const btn_avaliacao = document.getElementById('btn_navperfil-avaliacao');
+
+const sessao_sobre = document.querySelector('.sobre');
+const sessao_avaliacao = document.querySelector('.avaliacao');
+
+function clicou_contatos_redes() {
+    if (!btn_sobre.classList.contains('ativo')) {
+        document.querySelector('.button-navper.ativo').classList.remove('ativo');
+        btn_sobre.classList.add('ativo');
+
+        document.querySelector('.sessao').classList.remove('sessao');
+        sessao_sobre.classList.add('sessao');
+    }
+}
+
+function clicou_avaliacao() {
+    if (!btn_avaliacao.classList.contains('ativo')) {
+        document.querySelector('.button-navper.ativo').classList.remove('ativo');
+        btn_avaliacao.classList.add('ativo');
+
+        document.querySelector('.sessao').classList.remove('sessao');
+        sessao_avaliacao.classList.add('sessao');
+    }
+}
+
+btn_sobre.addEventListener('click', clicou_contatos_redes);
+btn_avaliacao.addEventListener('click', clicou_avaliacao);
+
+const btn_editarper = document.querySelector('.btn-editar-perfil');
+const fosco = document.querySelector('.fosco');
+
+function abrirEditarPerfil() {
+    fosco.classList.add('ativo');
+}
+
+const btn_fechar = document.querySelector('.cancelar_top');
+
+function fecharEditarPerfil() {
+    fosco.classList.remove('ativo');
+}
 
 // ====================== Parte 1 ======================
 async function verificarEmail(email) {
