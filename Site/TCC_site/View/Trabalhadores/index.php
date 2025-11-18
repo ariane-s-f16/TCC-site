9514ba987ac97a5ded64dc1ee31f797643927ebe
ariane-s-f16 <161371353+ariane-s-f16@users.noreply.github.com>
2025-11-17 23:23:38 -0300

    Add IDs to HTML elements for better accessibility

--- Site/TCC_site/View/Trabalhadores/index.php
+++ Site/TCC_site/View/Trabalhadores/index.php
@@ -84,8 +84,8 @@
                     </div>
 
                         <div class="nome-area">
-                            <h3 class="nome-card">Diego S. F.</h3>
-                            <p class="area-card"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-briefcase w-4 h-4">
+                            <h3 class="nome-card" id="nome">Diego S. F.</h3>
+                            <p class="area-card" id="area"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-briefcase w-4 h-4">
                                 <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path><rect width="20" height="14" x="2" y="6" rx="2"></rect>
                             </svg>
                             Desenvolvedor Front-end</p>
@@ -98,7 +98,7 @@
                         </button>
                     </div>
 
-                    <p class="empresa-ou-profissional">Profissional</p>
+                    <p class="empresa-ou-profissional"id="tipo">Profissional</p>
 
                     <div class="avaliacao-content">
                         <div class="flex items-center gap-1">
@@ -111,17 +111,17 @@
                         <span class="quant-avaliacoes">(203 avaliações)</span>
                     </div>
 
-                    <div class="localizacao" >
+                    <div class="localizacao" id="cidade" >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin w-4 h-4"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"></path><circle cx="12" cy="10" r="3"></circle></svg>
                         <span>Jaú, SP</span>
                     </div>
 
-                    <div class="telefone">
+                    <div class="telefone" id="telefone">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-4 h-4"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
                         <span>(14) 99121-1029</span>
                     </div>
 
-                    <div class="email">
+                    <div class="email" id="email">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-4 h-4"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
                         <span>diegosf0104@gmail.com</span>
                     </div>
